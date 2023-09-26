b3dcde108f95d2c32ef038b202b977d321669ca8
Leo Silva <leocardosodev@gmail.com>
2023-09-26 11:42:06 -0300

    feat: api get e post create  passengers

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,8 @@
 // @ts-nocheck
 import express from "express";
 import Ride from "./ride";
+import pgp from "pg-promise";
+import crypto from "crypto"
 
 const app = express();
 app.use(express.json());
@@ -13,8 +15,26 @@ app.post("/calculate_ride", function (req, res) {
     }
     const price = ride.calculate();
     return res.json({price});
-  } catch (error) {
+  } catch (error: any) {
     res.status(422).send(error.message)
   }
 });
+
+app.post("/passengers", async function(req, res) {
+  const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+  const passengerId = crypto.randomUUID();
+  await connection.query("insert into cccat12.passenger (passenger_id, name, email, document) values ($1, $2, $3, $4)", [
+    passengerId, req.body.name, req.body.email, req.body.document
+  ]);
+  await connection.$pool.end();
+  res.json({passengerId});
+});
+
+app.get("/passengers/:passengerId", async function(req, res){
+  const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+  const [passengerData] = await connection.query("select * from cccat12.passenger where passenger_id = $1",[req.params.passengerId]);
+  await connection.$pool.end();
+  res.json(passengerData);
+});
+
 app.listen(3000);
--- test/api.test.ts
+++ test/api.test.ts
@@ -26,3 +26,29 @@ test("Deve lançar um erro se a distancia for invalida", async () => {
   const output = response.data;
   expect(output).toBe("Invalid distance");
 });
+
+test("Deve cadastrar um passageiro", async () => {
+  const input = {
+    name: "John Doe",
+    email: "john.doe@test.com",
+    document: "834.326.160-74"
+  };
+  const response = await axios.post("http://localhost:3000/passengers", input);
+  const output = response.data;
+  expect(output.passengerId).toBeDefined();
+});
+
+test("Deve obter um passageiro", async () => {
+  const input = {
+    name: "John Doe",
+    email: "john.doe@test.com",
+    document: "834.326.160-74"
+  };
+  const responseCreatePassender = await axios.post("http://localhost:3000/passengers", input);
+  const outputCreatePassender = responseCreatePassender.data;
+  const response = await axios.get(`http://localhost:3000/passengers/${outputCreatePassender.passengerId}`);
+  const output = response.data;
+  expect(output.name).toBe("John Doe");
+  expect(output.email).toBe("john.doe@test.com");
+  expect(output.document).toBe("834.326.160-74");
+});
