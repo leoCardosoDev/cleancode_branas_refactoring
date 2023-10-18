38aeeb291b6174620605c245fecfae19405687fe
leoCardosoDev <leocardosodev@gmail.com>
2023-10-18 15:54:01 -0300

    feat: add fetch adpter

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,10 +3,10 @@ import './style.css'
 import App from './App.vue'
 import PassengerGatewayHttp from './infra/gateway/passenger_gateway_http'
 import DriverGatewayHttp from './infra/gateway/driver_gateway_http'
-import AxiosAdapter from './infra/http/axios_adapter'
+import FetchAdapter from './infra/http/fetch_adapter'
 
 const app = createApp(App)
-const httpClient = new AxiosAdapter()
+const httpClient = new FetchAdapter()
 app.provide('passengerGateway', new PassengerGatewayHttp(httpClient))
 app.provide('driverGateway', new DriverGatewayHttp(httpClient))
 app.mount('#app')
