4b91ed8dc306a1e3698f518618b07e1d013bdca7
leoCardosoDev <leocardosodev@gmail.com>
2023-10-18 07:23:07 -0300

    chore: change backend provided

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,33 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios'
+
+const name = ref('')
+const email = ref('')
+const document = ref('')
+const passengerId = ref('')
+
+async function createPassenger() {
+  const input = {
+    name: name.value,
+    email: email.value,
+    document: document.value
+  }
+  const response = await axios.post('http://localhost:3000/passengers', input)
+  const output = response.data
+  passengerId.value = output.passengerId
+}
+
 </script>
 
 <template>
-  <div><h1>Leo Silva</h1></div>
+  <div>
+    <input class="passenger-name" v-model="name">
+    <input class="passenger-email" v-model="email">
+    <input class="passenger-document" v-model="document">
+    <button class="create-passenger-button" @click="createPassenger()">Create Passenger</button>
+    <div class="passenger-id">{{ passengerId }}</div>
+  </div>
 </template>
 
 <style scoped>
--- frontend/test/App.test.ts
+++ frontend/test/App.test.ts
@@ -1,3 +1,11 @@
-test('Deve testar a aplicação', function() {
-  //
+import { mount } from '@vue/test-utils'
+import AppVue from '../src/App.vue'
+
+test('Deve testar a aplicação', async () => {
+  const wrapper = mount(AppVue, {})
+  await wrapper.get('.passenger-name').setValue('John Doe')
+  await wrapper.get('.passenger-email').setValue('john.doe@test.com')
+  await wrapper.get('.passenger-document').setValue('834.326.160-74')
+  await wrapper.get('.create-passenger-button').trigger('click')
+  expect(wrapper.get('.passenger-id').text()).toHaveLength(36)
 })
